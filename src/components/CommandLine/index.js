102ba89d9c5d1a5aa63252edd61639a9141bfce8
PAULO VICTOR CAETANO DE OLIVEIRA PR114067 <paulocivictor@gmail.com>
2021-12-01 14:33:14 -0300

    getting the char pressed in command line

--- src/components/CommandLine/index.js
+++ src/components/CommandLine/index.js
@@ -1,18 +1,27 @@
 import {useState} from 'react'
-import {CL} from './styles.js'
+import {CL_area, CL} from './styles.js'
 
 export default function CommandLine(){
     
     const comando = (e) => {
+        if(e.key === "Enter"){
+            alert("Foi o enter")
+        }
+        if(e.key === "Escape"){
+            alert("Foi o esc")
+        }
         
     }
 
     return(
         <>
-            <CL 
-                placeholder="Digite aqui o comando"
-                onKeyUp={comando()}
-            />
+            <CL_area>
+                <CL
+                    type="text" 
+                    placeholder="Digite aqui o comando"
+                    onKeyUp={comando}
+                />
+            </CL_area>
         </>
     )
 }
--- src/components/CommandLine/styles.js
+++ src/components/CommandLine/styles.js
@@ -1,7 +1,15 @@
 import styled from 'styled-components';
 
-export const CL = styled.input`
+export const CL_area = styled.div`
     position: fixed;
     bottom: 0px;
-    width:100%;
+    -webkit-box-sizing: border-box;
+    -moz-box-sizing: border-box;
+    box-sizing: border-box;
+    padding: 10px 20px 10px 10px;
+    width: 100%;
+`;
+
+export const CL = styled.input`
+    width: 100%;
 `;
